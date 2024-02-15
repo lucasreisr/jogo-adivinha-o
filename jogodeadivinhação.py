ed390880ee489603850872a92c14f7998c97b1fd
Lucas Reis <128156555+lucasreisr@users.noreply.github.com>
2024-02-15 09:50:18 -0300

    Add files via upload

---
+++ jogodeadivinhação.py
@@ -0,0 +1,16 @@
+
+print("********************************************************")
+print("Bem vindo, ao JOGO DE ADIVINHAÇÃO")
+print("*********************************************************")
+
+numeroSecreto = 17
+
+chute = input("Digite um número")
+
+print("Vocẽ  digitou o número", chute)
+
+if numeroSecreto == chute:
+   print("Vocẽ acertou")
+
+else:
+   print(" Vocẽ errou!")
