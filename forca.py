91a53e93b82eba67955fcc45e8ac751aa3e47963
Lucas Reis <128156555+lucasreisr@users.noreply.github.com>
2024-05-29 08:55:41 -0300

    Add files via upload

---
+++ forca.py
@@ -0,0 +1,24 @@
+def jogar_forca():
+
+    print('*********************************')
+    print('Bem vindo, ao JOGO FORCA')
+    print('*********************************')
+    #Definir qual a palavra secreta
+    palavraSecreta = "rainha"
+
+    enforcou = False
+    acertou = False
+
+
+    #Enquanto o jogador não se "enforcar" E não acertar a palavra, faça algo
+    while(not enforcou and not acertou):
+
+        chute= input("Escolha uma letra!")
+        print("Escolha outra letra!")
+        
+
+
+    print("Fim de jogo!")
+
+if(__name__ == "__main__"):
+    jogar_forca()
